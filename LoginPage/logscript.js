d857241f0799b7c66525ad6655a55cf74bdb035f
BeatrizBJorge <128194589+BeatrizBJorge@users.noreply.github.com>
2024-04-21 22:10:15 -0300

    Add files via upload

---
+++ LoginPage/logscript.js
@@ -0,0 +1,34 @@
+/* Login demonstrativo */
+/*
+function btnlog() {
+    var usuario = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+    var usuario_passou = (usuario == 'admin');
+    var senha_passou = (senha == 'senha');
+
+    if (usuario_passou && senha_passou) {
+        window.alert('Login bem-sucedido! Redirecionando...');
+        window.location.replace("../mainPage/mainpage.html");
+    } else {
+        window.alert("Credenciais inválidas. Tente novamente.");
+    }
+}*/
+
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    //obter os valores de usuario e senha
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    //validação do login para demonstração
+
+    // se o usuario e senha estiverem corretos, vai avançar para main page
+    if(usuario === "Admin" && senha === "admin1"){
+        alert("Login bem-sucedido! Redirecionando ...");
+        window.location.replace("../mainPage/mainpage.html")
+    }
+    else{
+        ("Usuario ou senha inválidos. Tente Novamente.");
+    }
+})
